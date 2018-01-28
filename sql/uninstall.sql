b7d334d914d6e0042c6fe32276f53524f2cb35a0
Pradeep Nayak <pradpnayak@gmail.com>
2018-01-28 17:36:44 +0530

    Delete Message template and respective option group and option values during uninstall

--- sql/uninstall.sql
+++ sql/uninstall.sql
@@ -4,3 +4,9 @@
 DROP TABLE IF EXISTS cdntaxreceipts_log_contributions;
 DROP TABLE IF EXISTS cdntaxreceipts_log;
 
+-- Delete message template and respective option group and option values
+DELETE  civicrm_option_value.*, civicrm_option_group.*, civicrm_msg_template.*
+FROM civicrm_option_value
+INNER JOIN civicrm_option_group ON  civicrm_option_value.option_group_id = civicrm_option_group.id
+INNER JOIN civicrm_msg_template ON civicrm_msg_template.workflow_id = civicrm_option_value.id
+WHERE civicrm_option_group.name = 'msg_tpl_workflow_cdntaxreceipts';
